5fab292c3112591e0e68af21280e0288df3bbd64
jwang19924 <jwang19924@gmail.com>
2017-04-26 22:15:33 -0400

    adding list of maps

--- views/listbuilder.js
+++ views/listbuilder.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
   var user = $("h1").text();
-  var res = user.substring(8, user.length);
+  var res = '+' + user.substring(8, user.length) + '+';
   console.log(res);
 });
