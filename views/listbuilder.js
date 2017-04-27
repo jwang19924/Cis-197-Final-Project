ab3f78ff49a0a599ca6e9f0d3dc45f91ae16bff4
jwang19924 <jwang19924@gmail.com>
2017-04-26 22:28:49 -0400

    added ajax for map lists

--- views/listbuilder.js
+++ views/listbuilder.js
@@ -14,6 +14,6 @@ var setupLists = function(username) {
 $(document).ready(function () {
   var user = $("h1").text();
   var res = user.substring(9, user.length);
-  console.log(res);
+  setupLists(user);
 
 });
