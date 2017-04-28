d3fa01cdc4697568993b9e363bb12226641a83dc
jwang19924 <jwang19924@gmail.com>
2017-04-27 22:02:37 -0400

    update syntax

--- app.js
+++ app.js
@@ -1,3 +1,139 @@
+// var express = require('express');
+
+// var app = express();
+
+// var cookieSession = require('cookie-session');
+// var bodyParser = require('body-parser');
+// var User = require('./user');
+// var Maps = require('./maps');
+
+// app.engine('html', require('ejs').__express);
+// app.set('view engine', 'html');
+
+// app.use(cookieSession({
+//   secret: 'SHHisASecret'
+// }));
+
+// app.use(bodyParser.urlencoded({extended: false}));
+
+// app.use('jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
+
+// app.get('/', function (req, res) {
+//   if (req.session.username && req.session.username !== '') {
+//     res.redirect('/protected');
+//   } else {
+//     res.redirect('/login');
+//   }
+// });
+
+// app.use('/', express.static('views'))
+
+// app.get('/login', function (req, res) {
+//   res.render('login');
+// });
+
+// app.post('/login', function(req, res) {
+//   username = req.body.username;
+//   password = req.body.password;
+//   User.checkIfLegit(username, password, null, function(err, isRight) {
+//     if (err) {
+//       res.send('Error! ' + err);
+//     } else {
+//       if (isRight) {
+//         req.session.username = username;
+//         res.redirect('/protected');
+//       } else {
+//         res.send('wrong password');
+//       }
+//     }
+//   });
+
+// });
+
+// app.get('/register', function (req, res) {
+//   res.render('register');
+// });
+
+// app.post('/register', function(req, res) {
+//   User.addUser(req.body.username, req.body.password, [], function(err) {
+//     if (err) res.send('error' + err);
+//     else res.send('new user registered with username ' + req.body.username);
+//   });
+// });
+
+// app.get('/logout', function(req, res) {
+//   req.session.username = '';
+//   res.render('logout');
+// });
+
+// app.get('/protected', function(req, res) {
+//   if (!req.session.username || req.session.username === '') {
+//     res.send('You tried to access a protected page');
+//   } else {
+//     res.render('protected', { username: req.session.username });
+//   }
+// });
+
+// app.post('/protected', function(req, res) {
+//   if (req.body.user) {
+//     User.getMapNames(req.body.user, function(err, nameofmaps) {
+//       res.send(nameofmaps);
+//     });
+//   }
+//   if (req.body.newmapname) {
+//     User.addMap(req.body.userToAddMap, req.body.newmapname, function (err) {
+//       console.log(err);
+//     });
+//     res.send('Created a new map!');
+//   }
+//   if (req.body.friendlist) {
+//     if (!req.body.friendlist) {
+//       res.send(null);
+//     }
+//     User.getMapNames(req.body.friendlist, function(err, nameofmaps) {
+//       res.send(nameofmaps);
+//     });
+//   }
+// });
+
+// app.get('/protected/:className/index', function (req, res) {
+//   if (!req.session.username || req.session.username === '') {
+//     res.send('You tried to access a protected page');
+//   } else {
+//     res.render('index', { username: req.params.className });
+//   }
+// });
+
+// app.post('/protected/:className/index', function (req, res) {
+//   var mapstring = req.body.mapstring;
+
+//   if (mapstring) {
+//     var array = mapstring.split("|");
+//     Maps.addMap(req.params.className, array, function(err) {
+//       if (err) {
+//         // map already exits 
+//         Maps.rewriteMap(req.params.className, array, function(err) {
+//           if (err) {
+//             next(err);
+//           }
+//         });
+//       }
+//     });
+//     res.send('Map has been saved!');
+//   } else {
+//     console.log('from express: user name is' + req.body.currentUser);
+//     Maps.getMap(req.body.currentUser, function (err, mapdata) {
+//       console.log("from express: " + mapdata);
+//       res.send(mapdata);
+//     });
+//   }
+// });
+
+// app.set('port', process.env.PORT || 3000);
+
+// app.listen(app.get('port'), function() { 
+//   console.log('listening');
+// });
 var express = require('express');
 
 var app = express();
@@ -26,16 +162,16 @@ app.get('/', function (req, res) {
   }
 });
 
-app.use('/', express.static('views'))
+app.use('/', express.static('views'));
 
 app.get('/login', function (req, res) {
   res.render('login');
 });
 
-app.post('/login', function(req, res) {
-  username = req.body.username;
-  password = req.body.password;
-  User.checkIfLegit(username, password, null, function(err, isRight) {
+app.post('/login', function (req, res) {
+  var username = req.body.username;
+  var password = req.body.password;
+  User.checkIfLegit(username, password, null, function (err, isRight) {
     if (err) {
       res.send('Error! ' + err);
     } else {
@@ -54,19 +190,19 @@ app.get('/register', function (req, res) {
   res.render('register');
 });
 
-app.post('/register', function(req, res) {
-  User.addUser(req.body.username, req.body.password, [], function(err) {
+app.post('/register', function (req, res) {
+  User.addUser(req.body.username, req.body.password, [], function (err) {
     if (err) res.send('error' + err);
     else res.send('new user registered with username ' + req.body.username);
   });
 });
 
-app.get('/logout', function(req, res) {
+app.get('/logout', function (req, res) {
   req.session.username = '';
   res.render('logout');
 });
 
-app.get('/protected', function(req, res) {
+app.get('/protected', function (req, res) {
   if (!req.session.username || req.session.username === '') {
     res.send('You tried to access a protected page');
   } else {
@@ -74,9 +210,9 @@ app.get('/protected', function(req, res) {
   }
 });
 
-app.post('/protected', function(req, res) {
+app.post('/protected', function (req, res) {
   if (req.body.user) {
-    User.getMapNames(req.body.user, function(err, nameofmaps) {
+    User.getMapNames(req.body.user, function (err, nameofmaps) {
       res.send(nameofmaps);
     });
   }
@@ -90,7 +226,7 @@ app.post('/protected', function(req, res) {
     if (!req.body.friendlist) {
       res.send(null);
     }
-    User.getMapNames(req.body.friendlist, function(err, nameofmaps) {
+    User.getMapNames(req.body.friendlist, function (err, nameofmaps) {
       res.send(nameofmaps);
     });
   }
@@ -108,14 +244,11 @@ app.post('/protected/:className/index', function (req, res) {
   var mapstring = req.body.mapstring;
 
   if (mapstring) {
-    var array = mapstring.split("|");
-    Maps.addMap(req.params.className, array, function(err) {
+    var array = mapstring.split('|');
+    Maps.addMap(req.params.className, array, function (err) {
       if (err) {
         // map already exits 
-        Maps.rewriteMap(req.params.className, array, function(err) {
-          if (err) {
-            next(err);
-          }
+        Maps.rewriteMap(req.params.className, array, function (err) {
         });
       }
     });
@@ -123,7 +256,7 @@ app.post('/protected/:className/index', function (req, res) {
   } else {
     console.log('from express: user name is' + req.body.currentUser);
     Maps.getMap(req.body.currentUser, function (err, mapdata) {
-      console.log("from express: " + mapdata);
+      console.log('from express: ' + mapdata);
       res.send(mapdata);
     });
   }
@@ -131,6 +264,6 @@ app.post('/protected/:className/index', function (req, res) {
 
 app.set('port', process.env.PORT || 3000);
 
-app.listen(app.get('port'), function() { 
+app.listen(app.get('port'), function () { 
   console.log('listening');
 });
